831c0fcf072f84fe0f193d1193396b5a4c8876f0
YatinDevs <c.yatin727@gmail.com>
2025-05-02 18:38:52 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import Home from "./pages/Home/Home";
 import PageNotFound from "./pages/ErrorPages/NotFound";
 import ServiceLayout from "./pages/Services/ServiceLayout";
 import TrainingSection from "./pages/Services/TrainingSection";
+import AboutUsPage from "./pages/AboutUs/AboutUsPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/about" element={<AboutUsPage />} />{" "}
         <Route path="/services/:slug" element={<ServiceLayout />} />{" "}
         <Route path="/services/training" element={<TrainingSection />} />{" "}
         <Route path="*" element={<PageNotFound />} />
